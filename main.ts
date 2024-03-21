766a9828ba977c9c90b96f9d77c6330c08e88a2e
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2024-03-21 22:08:16 +0500

    I made anumber guessing game

---
+++ main.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+
+   type ansType = {
+    userGuess: number
+   }
+
+const generatedNo = Math.floor(Math.random() * 10);
+
+const answers: ansType = await inquirer.prompt([{
+
+    type: "number",
+    name: "userGuess",
+    message: "Guess a number between 1 to 10: "
+}])
+
+const {userGuess} = answers;
+
+console.log(userGuess, "userGuess:", generatedNo, 'original number:')
+
+if (userGuess === generatedNo) {
+    console.log("You guessed right!")
+
+} else {
+    console.log("You guessed wrong!")
+}
+
+// console.log(generatedNo);
